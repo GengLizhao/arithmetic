e17a0f00b20305fb8f9bc160075a2dfc3349287f
genglizhao <genglizhao@100tal.com>
2020-09-24 14:40:10 +0800

    Initial Commit

---
+++ Solution/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Solution
+//
+//  Created by genglizhao on 2020/9/24.
+//  Copyright © 2020 genglizhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
