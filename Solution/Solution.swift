58212811f0bb422f3210cd779ab808d0d82fb0f7
genglizhao <genglizhao@100tal.com>
2020-09-24 15:11:41 +0800

    增加了平方算法

---
+++ Solution/Solution.swift
@@ -0,0 +1,13 @@
+//
+//  Pow.swift
+//  Solution
+//
+//  Created by genglizhao on 2020/9/24.
+//  Copyright © 2020 genglizhao. All rights reserved.
+//
+
+import UIKit
+
+class Pow: NSObject {
+
+}
